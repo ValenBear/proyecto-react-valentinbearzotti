aa039f00b4d5fe6109227122e2614c28a60a831c
Valen Bear <valenbear20101@gmail.com>
2023-07-10 21:17:26 -0300

    preentrega1final

--- src/componentes/CartWidget/index.jsx
+++ src/componentes/CartWidget/index.jsx
@@ -2,9 +2,15 @@ import './CartWidget.css'
 
 
 const CartWidget = () => {
+  const contador = 0;
+
+
   return (
     <>
+        <div className='div-cart'>
         <img src="../imagenes/carrito.png" alt="carrito de compras" />
+        <span className='contador'>{contador}</span>
+        </div>
     </>
   )
 }
